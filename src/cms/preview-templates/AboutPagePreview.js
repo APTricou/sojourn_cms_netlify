5e1b009b0377d1a7910ac11de2619b5d93ea7061
Antanas Tricou <APTricou@gmail.com>
2021-04-20 18:58:16 -0500

    About Page Preview for CMS changes

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -4,8 +4,9 @@ import { AboutPageTemplate } from '../../templates/about-page';
 
 const AboutPagePreview = ({ entry }) => {
   const data = entry.getIn(['data']).toJS();
+  const html = entry.getIn(['html']).toJS();
   if (data) {
-    return <AboutPageTemplate title={data.title} content={data.content} />;
+    return <AboutPageTemplate title={data.title} content={html} />;
   } else {
     return <div>Loading...</div>;
   }
