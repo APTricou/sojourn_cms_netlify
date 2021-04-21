ed41489290c899cc395482a1adee61bad283b3d1
Antanas Tricou <APTricou@gmail.com>
2021-04-20 19:08:59 -0500

    Services template fixes hopefully

--- src/cms/preview-templates/ServicesPagePreview.js
+++ src/cms/preview-templates/ServicesPagePreview.js
@@ -2,38 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ServicesPageTemplate } from '../../templates/services-page';
 
-const ServicesPagePreview = ({ entry, getAsset }) => {
-  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs']);
-  const blurbs = entryBlurbs ? entryBlurbs.toJS() : [];
-
-  const entryTestimonials = entry.getIn(['data', 'testimonials']);
-  const testimonials = entryTestimonials ? entryTestimonials.toJS() : [];
+const ServicesPagePreview = ({ entry }) => {
+  const data = entry.getIn(['data']).toJS();
 
   return (
     <ServicesPageTemplate
-      image={entry.getIn(['data', 'image'])}
-      title={entry.getIn(['data', 'title'])}
-      heading={entry.getIn(['data', 'heading'])}
-      description={entry.getIn(['data', 'description'])}
-      intro={{ blurbs }}
-      main={{
-        heading: entry.getIn(['data', 'main', 'heading']),
-        description: entry.getIn(['data', 'main', 'description']),
-        image1: {
-          image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image1', 'alt']),
-        },
-        image2: {
-          image: getAsset(entry.getIn(['data', 'main', 'image2', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image2', 'alt']),
-        },
-        image3: {
-          image: getAsset(entry.getIn(['data', 'main', 'image3', 'image'])),
-          alt: entry.getIn(['data', 'main', 'image3', 'alt']),
-        },
-      }}
-      fullImage={entry.getIn(['data', 'full_image'])}
-      testimonials={testimonials}
+      image={data.image}
+      title={data.title}
+      heading={data.heading}
+      description={data.description}
+      intro={data.intro || { blurbs: [] }}
+      main={data.main}
+      fullImage={data.fullImage}
+      testimonials={data.testimonials || []}
     />
   );
 };
